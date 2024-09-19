fae15ab3243fbc26e65a4a1c93e2452c6af838b5
Ibeth Rios <ibethriosmanchego@gmail.com>
2024-09-19 18:14:07 -0500

    Subiendo cambios en el index, estilos

--- Server.js
+++ Server.js
@@ -9,7 +9,7 @@ app.use(express.json());
 const db = mysql.createConnection({
     host: 'localhost',
     user: 'root',
-    password: 'password',
+    password: 'Ab123456',
     database: 'equipo_db'
 });
 
